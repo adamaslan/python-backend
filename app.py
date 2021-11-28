c76753b7e5382e9529c3683a0bb2767dd280ce42
Patrick Watson <pwatson@wayfair.com>
2021-11-28 16:55:59 -0500

    fixed json and put/delete/update

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_restful import reqparse, Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/postgres'
+# @todo Use env vars to use local or docker connection string
+# Docker uri for database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/postgres'
+# Localhost uri for database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:8001/postgres'
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 class Idea(db.Model):
@@ -15,6 +21,15 @@ class Idea(db.Model):
     def __repr__(self):
         return '<Idea %r>' % self.idea
 
+
+class IdeaSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id", "idea",)
+
+
+idea_schema = IdeaSchema()
+ideas_schema = IdeaSchema(many=True)
 
 try:
     db.create_all()
@@ -28,32 +43,35 @@ parser.add_argument('idea')
 
 class IdeaController(Resource):
     def get(self, idea_id):
-        return Idea.query.get(idea_id)
+        return idea_schema.dump(Idea.query.get(idea_id))
 
     def delete(self, idea_id):
         Idea.query.filter(Idea.id == idea_id).delete()
+        db.session.commit()
         return '', 204
 
     def put(self, idea_id):
         args = parser.parse_args()
 
-        Idea.query.get(idea_id).update({
+        Idea.query.filter(Idea.id == idea_id).update({
             Idea.idea: args['idea']
         })
 
-        return Idea.query.get(idea_id), 201
+        db.session.commit()
+        return self.get(idea_id), 201
 
 
 class IdeasController(Resource):
     def get(self):
-        return Idea.query.all()
+        ideas = Idea.query.all()
+        return ideas_schema.dump(ideas)
 
     def post(self):
         args = parser.parse_args()
         new_idea = Idea(idea=args['idea'])
         db.session.add(new_idea)
         db.session.commit()
-        return new_idea, 201
+        return idea_schema.dump(new_idea), 201
 
 
 ##
